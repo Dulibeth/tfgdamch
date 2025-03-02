8a6d90004b69a0fa969beec8186ad0fc74d414c3
Dulibeth <dulibeth.medinachirinos@usp.ceu.es>
2025-03-02 18:08:18 +0100

    Landing page inicial

--- app/app.js
+++ app/app.js
@@ -8,6 +8,7 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const loginRouter = require('./routes/login');
 const registerRouter = require('./routes/register');
+const landingRouter = require('./routes/landing');
 
 const app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/register', registerRouter);
+app.use('/landing', landingRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
